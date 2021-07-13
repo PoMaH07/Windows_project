7a47f51dce9431c7327e27a49d500287a1af6135
Roman <gio92641@gmail.com>
2021-07-13 16:15:08 +0300

    project windows

---
+++ src/js/modules/forms.js
@@ -0,0 +1,63 @@
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
+    const form = document.querySelectorAll('form');
+    const inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
+
+    const messege = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то  пошло не так'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = messege.loading;
+
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+            
+            let statusMessege = document.createElement('div');
+            statusMessege.classList.add('status');
+            item.appendChild(statusMessege);
+
+            const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessege.textContent = messege.success;
+                })
+                .catch(() => statusMessege.textContent = messege.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessege.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
